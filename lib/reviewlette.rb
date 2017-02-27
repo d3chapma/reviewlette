54a6c149bdbbc427a067a6acc21951a79c58518f
David Chapman <talk2dc@gmail.com>
2017-02-26 23:02:30 -0500

    Added automation for review link and PRs

--- lib/reviewlette.rb
+++ lib/reviewlette.rb
@@ -2,6 +2,10 @@ require 'reviewlette/trello_connection'
 require 'reviewlette/github_connection'
 require 'yaml'
 
+ENV['TRELLO_BOARD_ID'] = 'sYFl6KwF'
+ENV['TRELLO_KEY'] =      '0e1ff48869eaf4dd6b31631a5c506f9f'
+ENV['TRELLO_TOKEN'] =    'b32a083349171ba8b236977c3ad531d5656488f5645ff4762dfa300963b6b305'
+
 class Reviewlette
   def initialize(testers:, reviewers:)
     @trello  = TrelloConnection.new
@@ -10,21 +14,39 @@ class Reviewlette
   end
 
   def run
-    comment_for_review
+    check_under_review
   end
 
-  def comment_for_review
+  def check_under_review
     col = @trello.find_column('Under Review')
     col.cards.each do |card|
+      check_for_pr(card)
       comment_reviewers(card, @reviewers, 'Reviewer Assigned')
     end
   end
 
-  def comment_for_test
-    col = @trello.find_column('Testing')
-    col.cards.each do |card|
-      comment_testers(card, @testers, 'Tester Assigned')
+  def check_for_pr(card)
+    attachment = card.attachments.detect { |a| a.url.match(/github/) }
+    if attachment
+      add_review_link(card, attachment)
+    else
+      remind_owners_of_pr(card)
     end
+  end
+
+  def add_review_link(card, attachment)
+    has_link = card.desc.match(/herokuapp/)
+    unless has_link
+      _, pr = attachment.name.match(/^#(\w+)/).to_a
+      card.desc = "# https://teldiod3test-pr-#{pr}.herokuapp.com \n\n ----------\n\n #{card.desc}"
+      card.save
+    end
+  end
+
+  def remind_owners_of_pr(card)
+    owners = card.members.map(&:username)
+    handles = join_handles(owners)
+    card.add_comment("#{handles}, please attach a pull request using the GitHub button on the right.")
   end
 
   def comment_reviewers(card, reviewers, marker_label)
@@ -37,21 +59,13 @@ class Reviewlette
     card.add_label(label)
   end
 
-  def comment_testers(card, testers, marker_label)
-      return if card.labels.any? { |l| l.name == marker_label }
-
-      handles = random_handles(card, testers)
-      card.add_comment("#{handles}, you have been randomly chosen to test this card")
-
-      label = @trello.find_label(marker_label)
-      card.add_label(label)
-    end
-
   def random_handles(card, handles, count = 1)
     members = card.members.map(&:username)
     available_handles = handles.reject { |r| members.include? r.trello_handle }
-    available_handles.sample(count).map do |h|
-      "@#{h.trello_handle}"
-    end.join(' and ')
+    join_handles(available_handles.sample(count).map(&:trello_handle))
+  end
+
+  def join_handles(handles)
+    handles.map { |handle| "@#{handle}" }.join(' and ')
   end
 end
